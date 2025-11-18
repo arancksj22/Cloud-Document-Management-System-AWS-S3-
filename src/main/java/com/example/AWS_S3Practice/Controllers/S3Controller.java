861d17bb5e98ab7b8fe2ce8988c0b3a0e6555e82
arancksj22 <arancksj@gmail.com>
2025-11-18 22:08:19 +0530

    service dependancy injection

--- src/main/java/com/example/AWS_S3Practice/Controllers/S3Controller.java
+++ src/main/java/com/example/AWS_S3Practice/Controllers/S3Controller.java
@@ -1,7 +1,16 @@
 package com.example.AWS_S3Practice.Controllers;
 
+import com.example.AWS_S3Practice.Services.S3Service;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/s3")
 public class S3Controller {
+
+    private final S3Service service;
+
+    public S3Controller(S3Service service) {
+        this.service = service;
+    }
 }
