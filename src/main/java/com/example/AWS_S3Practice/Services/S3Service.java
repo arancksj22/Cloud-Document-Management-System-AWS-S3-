355eb824014e7dcc74e925e5a30a410add89f693
arancksj22 <arancksj@gmail.com>
2025-11-18 22:00:41 +0530

    s3 template and dependancy injection

--- src/main/java/com/example/AWS_S3Practice/Services/S3Service.java
+++ src/main/java/com/example/AWS_S3Practice/Services/S3Service.java
@@ -1,7 +1,18 @@
 package com.example.AWS_S3Practice.Services;
 
+import io.awspring.cloud.s3.S3Template;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class S3Service {
+
+    private final S3Template s3Template;
+
+    public S3Service(S3Template s3Template) {
+        this.s3Template = s3Template;
+    }
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucketName;
 }
