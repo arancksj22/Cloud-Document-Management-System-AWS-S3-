30d1a7f0995fc82e8c4ce3f6b9361c13cf943daa
arancksj22 <arancksj@gmail.com>
2025-11-18 21:42:42 +0530

    basic boilerplate

---
+++ src/test/java/com/example/AWS_S3Practice/AwsS3PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AWS_S3Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsS3PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
