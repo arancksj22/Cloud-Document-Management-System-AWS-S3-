4ee1551d539d4a6e00d5ebd15302af3e3df2dc47
arancksj22 <arancksj@gmail.com>
2025-11-18 22:11:40 +0530

    post mapping

--- src/main/java/com/example/AWS_S3Practice/Controllers/S3Controller.java
+++ src/main/java/com/example/AWS_S3Practice/Controllers/S3Controller.java
@@ -1,8 +1,14 @@
 package com.example.AWS_S3Practice.Controllers;
 
 import com.example.AWS_S3Practice.Services.S3Service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/s3")
@@ -12,5 +18,10 @@ public class S3Controller {
 
     public S3Controller(S3Service service) {
         this.service = service;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam("file")MultipartFile file) throws IOException {
+        return ResponseEntity.ok(service.upload(file));
     }
 }
